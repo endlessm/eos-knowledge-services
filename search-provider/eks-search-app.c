6f7e3923359784948df025ac261488f82f6a457b
Matt Watson <mattdangerw@gmail.com>
2017-01-18 15:54:56 -0800

    eks-search-app: copy in systemd dbus unescape function
    Rather than rely on the library.
    https://phabricator.endlessm.com/T14392

--- search-provider/eks-search-app.c
+++ search-provider/eks-search-app.c
@@ -6,7 +6,7 @@
 #include "eks-search-provider-dbus.h"
 #include "eks-subtree-dispatcher.h"
 
-#include <systemd/sd-bus.h>
+#include <string.h>
 
 /**
  * EksSearchProvider:
@@ -76,6 +76,67 @@ eks_search_app_class_init (EksSearchAppClass *klass)
   application_class->dbus_unregister = eks_search_app_unregister;
 }
 
+// The following code is adapted from
+// https://github.com/systemd/systemd/blob/master/src/basic/bus-label.c
+static gint
+unhexchar (gchar c)
+{
+  if (c >= '0' && c <= '9')
+    return c - '0';
+
+  if (c >= 'a' && c <= 'f')
+    return c - 'a' + 10;
+
+  if (c >= 'A' && c <= 'F')
+    return c - 'A' + 10;
+
+  return -1;
+}
+
+static gchar *
+bus_label_unescape (const gchar *f) {
+  gchar *r, *t;
+  gsize i;
+  gsize l = f ? strlen (f) : 0;
+
+  /* Special case for the empty string */
+  if (l == 1 && *f == '_')
+    return g_strdup ("");
+
+  r = g_new (gchar, l + 1);
+  if (!r)
+    return NULL;
+
+  for (i = 0, t = r; i < l; ++i)
+    {
+      if (f[i] == '_')
+        {
+          int a, b;
+
+          if (l - i < 3 ||
+              (a = unhexchar (f[i + 1])) < 0 ||
+              (b = unhexchar (f[i + 2])) < 0)
+            {
+              /* Invalid escape code, let's take it literal then */
+              *(t++) = '_';
+            }
+          else
+            {
+              *(t++) = (gchar) ((a << 4) | b);
+              i += 2;
+            }
+        }
+      else
+        {
+          *(t++) = f[i];
+        }
+    }
+
+  *t = 0;
+
+  return r;
+}
+
 static GDBusInterfaceSkeleton *
 dispatch_subtree (EksSubtreeDispatcher *dispatcher,
                   const gchar *subnode,
@@ -84,9 +145,7 @@ dispatch_subtree (EksSubtreeDispatcher *dispatcher,
   EksSearchProvider *provider = g_hash_table_lookup (self->app_search_providers, subnode);
   if (provider == NULL)
     {
-      g_autofree gchar *app_id = NULL;
-      g_autofree gchar *prefixed = g_strconcat ("/", subnode, NULL);
-      sd_bus_path_decode (prefixed, "/", &app_id);
+      g_autofree gchar *app_id = bus_label_unescape (subnode);
       provider = g_object_new (EKS_TYPE_SEARCH_PROVIDER,
                                "application-id", app_id,
                                NULL);
