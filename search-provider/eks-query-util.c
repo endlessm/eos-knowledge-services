9b03c4d8253f88a732bb302eb0ee2980fb2f548a
Sam Spilsbury <sam@endlessm.com>
2018-03-09 19:39:41 +0800

    search-provider: Split out common querying functionality in eks-query-util.h

---
+++ search-provider/eks-query-util.c
@@ -0,0 +1,78 @@
+/* Copyright 2018 Endless Mobile, Inc. */
+
+#include "eks-query-util.h"
+
+#include <eos-knowledge-content.h>
+#include <eos-shard/eos-shard-shard-file.h>
+
+#include <gio/gio.h>
+
+gboolean
+models_for_result (EkncEngine    *engine,
+                   const char    *application_id,
+                   GAsyncResult  *result,
+                   GSList       **models,
+                   GError       **error)
+{
+  g_autoptr(EkncQueryResults) results = NULL;
+  if (!(results = eknc_engine_query_finish (engine, result, error)))
+      return FALSE;
+
+  EkncDomain *domain = eknc_engine_get_domain_for_app (engine,
+                                                       application_id,
+                                                       error);
+  if (domain == NULL)
+      return FALSE;
+
+  *models = g_slist_copy_deep (eknc_query_results_get_models (results),
+                               (GCopyFunc) g_object_ref,
+                               NULL);
+
+  return TRUE;
+}
+
+gboolean
+models_and_shards_for_result (EkncEngine    *engine,
+                              const char    *application_id,
+                              GAsyncResult  *result,
+                              GSList       **models,
+                              GSList       **shards,
+                              GError       **error)
+{
+  g_autoptr(EkncQueryResults) results = NULL;
+  if (!(results = eknc_engine_query_finish (engine, result, error)))
+      return FALSE;
+
+  EkncDomain *domain = eknc_engine_get_domain_for_app (engine,
+                                                       application_id,
+                                                       error);
+  if (domain == NULL)
+      return FALSE;
+
+  *shards = g_slist_copy_deep (eknc_domain_get_shards (domain),
+                               (GCopyFunc) g_object_ref,
+                               NULL);
+  *models = g_slist_copy_deep (eknc_query_results_get_models (results),
+                               (GCopyFunc) g_object_ref,
+                               NULL);
+
+  return TRUE;
+}
+
+GStrv
+strv_from_shard_list (GSList *string_list)
+{
+  GStrv strv = g_new0 (char *, g_slist_length (string_list));
+  guint count = 0;
+
+  for (GSList *l = string_list; l; l = l->next)
+    {
+      EosShardShardFile *shard = l->data;
+      char *shard_path = NULL;
+
+      g_object_get (shard, "path", &shard_path, NULL);
+      strv[count++] = shard_path;
+    }
+
+  return strv;
+}
