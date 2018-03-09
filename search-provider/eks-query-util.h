f05d0bc8f50605a9059186f6a938692bc7339418
Sam Spilsbury <sam@endlessm.com>
2018-03-09 19:39:41 +0800

    search-provider: Split out common querying functionality in eks-query-util.h

---
+++ search-provider/eks-query-util.h
@@ -0,0 +1,24 @@
+/* Copyright 2018 Endless Mobile, Inc. */
+
+#pragma once
+
+#include <eos-knowledge-content.h>
+#include <eos-shard/eos-shard-shard-file.h>
+
+#include <gio/gio.h>
+
+gboolean models_for_result (EkncEngine    *engine,
+                            const gchar   *application_id,
+                            GAsyncResult  *result,
+                            GSList       **models,
+                            GError       **error);
+
+gboolean models_and_shards_for_result (EkncEngine    *engine,
+                                       const gchar   *application_id,
+                                       GAsyncResult  *result,
+                                       GSList       **models,
+                                       GSList       **shards,
+                                       GError       **error);
+
+GStrv strv_from_shard_list (GSList *string_list);
+
